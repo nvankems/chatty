563b93d0c3ec49f0fad58809af3e68cf2781136b
nvankems <nvankems@yahoo.com>
2016-03-09 20:11:21 -0600

    done

--- server.js
+++ server.js
@@ -1,1 +1,52 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
+app.use(bodyParser.json());
+
+var messages = [{message: "poop", time: new Date()}];
+
+app.options('/', function(req, res) {
+    res.set({
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+    }).send()
+});
+
+app.post('/', function(req, res) {
+    messages.push({
+        message: req.body.message,
+        time: new Date()
+    });
+
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+
+
+});
+
+app.get('/', function(req, res) {
+    res.status(200).set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+        'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+        'X-XSS-Protection': '1; mode=block',
+        'X-Frame-Options': 'SAMEORIGIN',
+        'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+    }).send(JSON.stringify(messages));
+});
+
+
+
+app.listen(8095, function() {
+    console.log('Listening on 8095');
+});
